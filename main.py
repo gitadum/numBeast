fb0d58625f2b9350747210bbfd2f62d816fa9310
Adrien Dumont <dev@adum.io>
2022-07-18 19:04:59 +0200

    calculate & displayRes moved to main (func) script

--- main.py
+++ main.py
@@ -4,6 +4,7 @@
 # This program is made to train mental calculation skills
 # ie addition (+), substraction (-), multiplication (*) and division (/)
 
+import random
 from functools import reduce
 from datetime import datetime
 
@@ -44,3 +45,39 @@ def reckon(op, *terms):
         'flagOK': stt,
         'timeSc': tim,
             }
+
+def calculate(nTerms=(2,3), rangeTerm=(1,1000), operator="random"):
+    """
+    op : rank of the operation to be performed, a random number between 0 and 3
+    0: addition, 1: substraction, 2: multiplication, 3: division"
+    """
+    terms = []
+    for n in range(random.randint(*nTerms)):
+        terms.append(random.randint(*rangeTerm))
+    if operator == "random":
+        op = random.randint(0,3)
+    elif operator == "excludeDivision":
+        op = random.randint(0,2)
+    x = reckon(op, *terms)
+    return x
+
+def displayRes(x):
+    print(x["calcul"])
+    try:
+        assert x['flagOK'] == 1
+        print("Good answer.")
+    except AssertionError:
+        print("Bad answer.")
+        print(f"You answered {x['usrAns']}")
+        print(f"The answer was {x['corAns']}")
+    finally:
+        delta = str(x['timeSc']).replace(':','h',1).replace(':','m',1) + 's'
+        print(f"Done within {delta}.")
+
+def calculationSerie(n = 5):
+    serie = []
+    for i in range(5):
+        calc = calculate(operator="excludeDivision")
+        serie.append(calc)
+    for calc in serie:
+        displayRes(calc)
--- test.py
+++ test.py
@@ -1,32 +1,3 @@
-import random
-from main import reckon as reckonv2
+from main import calculationSerie
 
-t1 = random.randint(1,1000) # First term, random integer between 1 and 1,000
-t2 = random.randint(1,1000) # Second term, random integer between 1 and 1,000
-op = random.randint(0,3) # rank of the operation to be performed
-                         # a random number between 0 and 3
-                         # 0: addition, 1: substraction,
-                         # 2: multiplication, 3: division
-
-def calculate(nTerms=random.randint(2,3), rangeTerms=random.randint(1,1000),
-              operator="random"):
-    terms = []
-    for n in range(nTerms):
-        terms.append(rangeTerms)
-    print(terms)
-    if operator == "random":
-        op = random.randint(0,3)
-    elif operator == "excludeDivision":
-        op = random.randint(0,2)
-    x = reckonv2(op, *terms)
-    try:
-        assert x['flagOK'] == 1
-        print("Good answer.")
-    except AssertionError:
-        print("Bad answer.")
-        print(f"The answer was {x['corAns']}")
-    finally:
-        delta = str(x['timeSc']).replace(':','h',1).replace(':','m',1) + 's'
-        print(f"Done within {delta}.")
-
-calculate(operator="excludeDivision")
+calculationSerie()
