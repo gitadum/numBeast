c1aefc3c74af337f101171e8f3a134ad0b2a6bf1
Adrien Dumont <dev@adum.io>
2022-07-18 18:26:28 +0200

    new caclculate func to tweak the calculation terms

--- test.py
+++ test.py
@@ -8,20 +8,25 @@ op = random.randint(0,3) # rank of the operation to be performed
                          # 0: addition, 1: substraction,
                          # 2: multiplication, 3: division
 
-nTerms = random.randint(2,3)
-terms = []
-for n in range(nTerms):
-    terms.append(random.randint(1,1000))
-print(terms)
+def calculate(nTerms=random.randint(2,3), rangeTerms=random.randint(1,1000),
+              operator="random"):
+    terms = []
+    for n in range(nTerms):
+        terms.append(rangeTerms)
+    print(terms)
+    if operator == "random":
+        op = random.randint(0,3)
+    elif operator == "excludeDivision":
+        op = random.randint(0,2)
+    x = reckonv2(op, *terms)
+    try:
+        assert x['flagOK'] == 1
+        print("Good answer.")
+    except AssertionError:
+        print("Bad answer.")
+        print(f"The answer was {x['corAns']}")
+    finally:
+        delta = str(x['timeSc']).replace(':','h',1).replace(':','m',1) + 's'
+        print(f"Done within {delta}.")
 
-x = reckonv2(op, *terms)
-
-try:
-    assert x['flagOK'] == 1
-    print("Good answer.")
-except AssertionError:
-    print("Bad answer.")
-    print(f"The answer was {x['corAns']}")
-finally:
-    delta = str(x['timeSc']).replace(':','h',1).replace(':','m',1) + 's'
-    print(f"Done within {delta}.")
+calculate(operator="excludeDivision")
