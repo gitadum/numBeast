1d1e043843b734b09e20006e57e9bf9db87bf874
Adrien Dumont <dev@adum.io>
2022-07-29 14:48:39 +0200

    removed test script

--- test.py
+++
@@ -1,6 +0,0 @@
-from func import calculationSerie, multiplicationTables
-
-calculationSerie(n=5, operator="onlyAdditionOrSubstaction")
-calculationSerie(n=5, nTerms=2, rangeTerm=(1,100),
-                 operator="onlyMultiplicationOrDivision")
-multiplicationTables()
