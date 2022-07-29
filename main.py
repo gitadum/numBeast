91b5f07a419aff59d9d1e2dbdef714b626fdec46
Adrien Dumont <dev@adum.io>
2022-07-29 13:38:21 +0200

    created a selectRandomInt func enhanced the display results function created a multiplication table func

--- main.py
+++ main.py
@@ -7,6 +7,8 @@
 import random
 from functools import reduce
 from datetime import datetime
+from timer import timer
+
 
 def reckon(op, *terms):
     """
@@ -46,38 +48,69 @@ def reckon(op, *terms):
         'timeSc': tim,
             }
 
+
+def selectRandomInt(lowBound=0, upBound=3, exclude=None):
+    while True:
+        y = random.randint(lowBound, upBound)
+        try:
+            assert type(exclude) != list
+            if y != exclude: break
+        except AssertionError:
+            if y not in exclude: break
+    return y
+
+
 def calculate(nTerms=(2,3), rangeTerm=(1,1000), operator="random"):
     """
     op : rank of the operation to be performed, a random number between 0 and 3
     0: addition, 1: substraction, 2: multiplication, 3: division"
     """
     terms = []
-    for n in range(random.randint(*nTerms)):
-        terms.append(random.randint(*rangeTerm))
+    for n in range(selectRandomInt(*nTerms)):
+        terms.append(selectRandomInt(*rangeTerm))
     if operator == "random":
-        op = random.randint(0,3)
+        op = selectRandomInt()
+    elif operator == "excludeMultiplication":
+        op = selectRandomInt(exclude=2)
+    elif operator == "onlyMultiplication":
+        op = 2
     elif operator == "excludeDivision":
-        op = random.randint(0,2)
+        op = selectRandomInt(exclude=3)
+    elif operator == "onlyDivision":
+        op = 3
     x = reckon(op, *terms)
     return x
 
+
 def displayRes(x):
-    print(x["calcul"])
+    msg = ""
     try:
         assert x['flagOK'] == 1
-        print("Good answer.")
+        msg += x["calcul"] + f"{x['usrAns']}"
+        msg += " " + "Good answer."
     except AssertionError:
-        print("Bad answer.")
-        print(f"You answered {x['usrAns']}")
-        print(f"The answer was {x['corAns']}")
+        msg += x["calcul"].replace(" = ", "") + " =/= " + f"{x['usrAns']}"
+        msg += " " + "Bad answer."
+        msg += " " + f"The answer was {x['corAns']}."
     finally:
         delta = str(x['timeSc']).replace(':','h',1).replace(':','m',1) + 's'
-        print(f"Done within {delta}.")
+        msg += " " + f"Done within {delta}."
+        print(msg)
 
-def calculationSerie(n = 5):
+
+@timer
+def calculationSerie(n = 5, *args, **kwargs):
+    decorLine = 8 * "-"
     serie = []
-    for i in range(5):
-        calc = calculate(operator="excludeDivision")
+    for i in range(n):
+        calc = calculate(*args, **kwargs)
         serie.append(calc)
+    print(decorLine)
     for calc in serie:
         displayRes(calc)
+    print(decorLine)
+
+
+def multiplicationTables(n=10, multipleRange=(1,12)):
+    calculationSerie(n=n, nTerms=(2,2), rangeTerm=multipleRange, 
+                     operator="onlyMultiplication")
